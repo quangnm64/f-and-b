16eeb50fcf563f5a849b5c9a6200f4f404326bd7
quangnm64 <quang.nm.64cntt@ntu.edu.vn>
2025-12-10 15:17:11 +0700

    updated

--- src/app/api/order/route.ts
+++ src/app/api/order/route.ts
@@ -1,8 +1,8 @@
+// import { createOrder } from '@/modules/order/service';
 import { NextResponse } from "next/server";
-import { createOrder } from "@/modules/order/service";
 
-export async function POST(req: Request) {
-  const data = await req.json();
-  const res = await createOrder(data);
-  return NextResponse.json(res);
-}
+// export async function POST(req: Request) {
+//   const data = await req.json();
+//   const res = await createOrder(data);
+//   return NextResponse.json(res);
+// }
